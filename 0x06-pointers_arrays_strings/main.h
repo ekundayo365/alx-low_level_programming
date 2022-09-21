f478240936459143ae1fdd5a9d87ad89b95993e0
ekundayo365 <damola.ekundayo@gmail.com>
2022-09-21 19:53:13 +0100

    Update task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,23 +1,20 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * File: main.h
- * Auth: Courtesy of ALX
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
- */
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
 
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
+char *string_toupper(char *s);
+char *cap_string(char *s);
+char *leet(char *s);
+char *rot13(char *s);
+int _putchar(char c);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
