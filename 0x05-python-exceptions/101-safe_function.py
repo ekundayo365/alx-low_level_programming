14c850ab43ef2f262c351199dba92d7203ec330c
ekundayo365 <damola.ekundayo@gmail.com>
2022-12-19 19:39:11 +0100

    Attempted all tasks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 import sys
 
 
 def safe_function(fct, *args):
     try:
         result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
+    return result
